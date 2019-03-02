1cc460e5289ff4d9d6c30e5222d87cecf0155397
Unknown <kizy.snowy@hotmail.co.uk>
2019-03-02 17:14:23 +0000

    Added slider to testimonial.

--- src/js/modules/_flickity.js
+++ src/js/modules/_flickity.js
@@ -13,7 +13,28 @@ var flkty = new Flickity(item_brand, ({
     autoPlay: 3000,
 }), 'groupCells', 4);
 
-// NOTE: Testimonial-Slider
-
-
 // NOTE: Feature-Slider
+var item_feature = document.querySelector('.casestudy-block__list');
+
+var flkty = new Flickity(item_feature, ({
+    // options
+    prevNextButtons: true,
+    pageDots: false,
+    contain: true,
+    freeScroll: true,
+    wrapAround: true,
+    autoPlay: 10000,
+}), 'groupCells', 1);
+
+// NOTE: Testimonial-Slider
+var item_testimonial = document.querySelector('.testimonial-slider__slider');
+
+var flkty = new Flickity(item_testimonial, ({
+    // options
+    prevNextButtons: true,
+    pageDots: false,
+    contain: true,
+    freeScroll: true,
+    wrapAround: true,
+    autoPlay: 10000,
+}), 'groupCells', 1);
