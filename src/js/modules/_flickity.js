7f92ac2df97939e08c9914f3f89c77c276e0cca6
Unknown <kizy.snowy@hotmail.co.uk>
2019-03-03 14:55:27 +0000

    Fixed flickity issue

--- src/js/modules/_flickity.js
+++ src/js/modules/_flickity.js
@@ -1,40 +1,50 @@
 var Flickity = require('flickity');
 
-// NOTE: Brands-Slider
-var item_brand = document.querySelector('.brands-slider__container');
+// A $( document ).ready() block.
+$( document ).ready(function() {
+    console.log( "ready!" );
 
-var flkty = new Flickity(item_brand, ({
-    // options
-    prevNextButtons: true,
-    pageDots: false,
-    contain: true,
-    freeScroll: true,
-    wrapAround: true,
-    autoPlay: 3000,
-}), 'groupCells', 4);
+    // NOTE: Brands-Slider
+    var item_brand = document.querySelector('.brands-slider__container');
 
-// NOTE: Feature-Slider
-var item_feature = document.querySelector('.casestudy-block__list');
+    var flkty = new Flickity(item_brand, ({
+        // options
+        prevNextButtons: false,
+        pageDots: false,
+        contain: true,
+        freeScroll: true,
+        wrapAround: true,
+        autoPlay: 3000,
+        imagesLoaded: true,
+    }), 'groupCells', 4);
 
-var flkty = new Flickity(item_feature, ({
-    // options
-    prevNextButtons: true,
-    pageDots: false,
-    contain: true,
-    freeScroll: true,
-    wrapAround: true,
-    autoPlay: 10000,
-}), 'groupCells', 1);
+    // NOTE: Feature-Slider
+    var item_feature = document.querySelector('.casestudy-block__list');
 
-// NOTE: Testimonial-Slider
-var item_testimonial = document.querySelector('.testimonial-slider__slider');
+    var flkty = new Flickity(item_feature, ({
+        // options
+        prevNextButtons: true,
+        pageDots: false,
+        contain: true,
+        freeScroll: true,
+        wrapAround: true,
+        draggable: false,
+        autoPlay: 10000,
+        imagesLoaded: true,
+    }), 'groupCells', 1);
 
-var flkty = new Flickity(item_testimonial, ({
-    // options
-    prevNextButtons: true,
-    pageDots: false,
-    contain: true,
-    freeScroll: true,
-    wrapAround: true,
-    autoPlay: 10000,
-}), 'groupCells', 1);
+    // NOTE: Testimonial-Slider
+    var item_testimonial = document.querySelector('.testimonial-slider__slider');
+
+    var flkty = new Flickity(item_testimonial, ({
+        // options
+        prevNextButtons: true,
+        pageDots: false,
+        contain: true,
+        freeScroll: true,
+        wrapAround: true,
+        draggable: false,
+        autoPlay: 10000,
+        imagesLoaded: true,
+    }), 'groupCells', 1);
+});
